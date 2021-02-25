3a826e44350ae6867719cb07fe236da22eb2c309
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-02-25 16:36:44 +0530

    added alert and clear list

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-export default function Alert() {
-  return (
-    <h2>Alert</h2>
-  );
+export default function Alert({ type, msg, removeAlert }) {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
 }
--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import List from './List'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 import "./styles.css";
 
 export default function App() {
-  const [name, setName] = useState('');
-  const [list,setList] = useState([]);
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
-  const [alert, setAlert] = useState({show:false, msg:'',type:''});
-  
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
-      
+      showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
-
     } else {
-      const newItem = {id: new Date().getTime().toString(), title:name};
+      showAlert(true, "success", "item added to list");
+      const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
-      setName('');
+      setName("");
     }
-  }
-  
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  };
+
   return (
     <section className="section-center">
       <form className="list-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>Todo List</h3>
         <div className="form-control">
-          <input type="text" className="list" placeholder="buy ..." value={name} onChange={(e) => {setName(e.target.value)}} />
+          <input
+            type="text"
+            className="list"
+            placeholder="buy ..."
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
           <button type="submit" className="submit-btn">
-            {isEditing ? 'edit' : 'submit'}
+            {isEditing ? "edit" : "submit"}
           </button>
         </div>
       </form>
       {list.length > 0 && (
         <div className="list-container">
           <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
